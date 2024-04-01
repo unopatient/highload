3c915219b10d53f2c0612c3eee10c4b6cd3493bd
bonflower <54567022+asaphbay@users.noreply.github.com>
2024-04-01 00:51:35 -0700

    rank32() test, lower_bound() impl

--- order-book/src/btree.rs
+++ order-book/src/btree.rs
@@ -29,16 +29,44 @@ fn rank32(x: __m256i, node: *const i32) -> u32 {
 const R: usize = 100_000_000;
 const B: usize = 32;
 
-#[repr(align(64))]
+#[repr(C, align(64))]
 struct BTreeMap {
-    pub tree: [i32; R]
+    tree: [i32; R],
+    root: usize,
+    n_tree: i32,
+    height: i32
 }
 
 impl BTreeMap {
-    fn new() -> Self  {
-        Self {
-            tree: [0; R]
+    fn new() -> Self {
+        let mut tree = [0; R];
+
+        // prepare() 
+        for i in 0..R {
+            tree[i] = i32::MAX;
         }
+
+        Self {
+            tree,
+            root: 0,
+            n_tree: B as i32,
+            height: 1
+        }
+    }
+
+    fn lower_bound(&self, _x: i32) -> i32 {
+        let mut k = self.root;
+
+        let x = unsafe{_mm256_set1_epi32(_x)};
+
+        for _ in 0..self.height-1 {
+            let i = rank32(x, &self.tree[k]) as usize;
+            k = self.tree[k + B + i] as usize;
+        }
+
+        let i = rank32(x, &self.tree[k]) as usize;
+
+        self.tree[k + i]
     }
 }
 
@@ -46,29 +74,64 @@ impl BTreeMap {
 mod tests {
 
     use super::*;
+    use rand::seq::SliceRandom;
 
     #[repr(align(64))]
     struct TestArray(pub [i32; 64]);
 
     #[test]        
     fn test_cmp() {
+        // Set first 8 elements to 1 to 8 (inclusive)
         let mut array = TestArray([0; 64]);
-        array.0[0..8].copy_from_slice(&[3,6,1,2,5,7,8,4]);
-        let array_ptr = array.0.as_ptr();
+        array.0[0..8].copy_from_slice(&(1..9).collect::<Vec<i32>>());
 
-        let all_bytes_zero = unsafe {_mm256_setzero_si256()};
+        let mut rng = rand::thread_rng();
 
-        // check that cmp returns the number of elements LESS than i
-        for i in 1..9 {
-            let saturated_mask = unsafe {
-                let all_bytes_i = _mm256_set1_epi32(i);
-                let mut cmp_mask = cmp(all_bytes_i, array_ptr);
-                cmp_mask = _mm256_blend_epi16(cmp_mask, all_bytes_zero, 0b01010101);
-                cmp_mask = _mm256_packs_epi16(cmp_mask, all_bytes_zero);
-                _mm256_movemask_epi8(cmp_mask)
-            };
+        // 100 trials
+        for _ in 0..100 {
+            // And shuffle the first 8 elements
+            array.0[0..8].shuffle(&mut rng);
 
-            assert_eq!(i - 1, saturated_mask.count_ones() as i32);
+            let array_ptr = array.0.as_ptr();
+
+            let all_bytes_zero = unsafe {_mm256_setzero_si256()};
+
+            // check that cmp returns the number of elements LESS than i
+            for i in 1..9 {
+                let saturated_mask = unsafe {
+                    let all_bytes_i = _mm256_set1_epi32(i);
+                    let mut cmp_mask = cmp(all_bytes_i, array_ptr);
+                    cmp_mask = _mm256_blend_epi16(cmp_mask, all_bytes_zero, 0b01010101);
+                    cmp_mask = _mm256_packs_epi16(cmp_mask, all_bytes_zero);
+                    _mm256_movemask_epi8(cmp_mask)
+                };
+
+                assert_eq!((i - 1) as u32, saturated_mask.count_ones());
+            }
+        }
+    }
+
+    #[test]
+    fn test_rank32() {
+        // Set first 32 elements to 1 to 32 (inclusive)
+        let mut array = TestArray([0; 64]);
+        array.0[0..32].copy_from_slice(&(1..33).collect::<Vec<i32>>());
+
+        let mut rng = rand::thread_rng();
+
+        // 100 trials
+        for _ in 0..100 {
+            // And shuffle the first 32 elements
+            array.0[0..32].shuffle(&mut rng);
+
+            let array_ptr = array.0.as_ptr();
+
+            // check that rank32 returns the number of elements LESS than i
+            for i in 1..33 {
+                let all_bytes_i = unsafe{_mm256_set1_epi32(i)};
+                let count_lt_i = rank32(all_bytes_i, array_ptr);
+                assert_eq!((i - 1) as u32, count_lt_i);
+            }
         }
     }
 }
