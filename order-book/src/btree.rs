691f9a785907b9edf94266a9ba26e8d40fc8b6c4
bonflower <54567022+asaphbay@users.noreply.github.com>
2024-04-02 22:55:44 -0700

    test move_latter_half() impl Precalc, node-level insert()

--- order-book/src/btree.rs
+++ order-book/src/btree.rs
@@ -2,6 +2,9 @@
 
 use core::arch::x86_64::*;
 
+const R: usize = 100_000_000;
+const B: usize = 32;
+
 fn cmp(x: __m256i, node: *const i32) -> __m256i {
     unsafe {
         let y = _mm256_load_si256(node as *const __m256i);
@@ -26,8 +29,58 @@ fn rank32(x: __m256i, node: *const i32) -> u32 {
     unsafe {_popcnt32(mask) as u32}
 }
 
-const R: usize = 100_000_000;
-const B: usize = 32;
+fn move_latter_half(from: *mut i32, to: *mut i32) {
+    let infs: __m256i = unsafe{_mm256_set1_epi32(i32::MAX)};
+
+    for i in (0..B/2).step_by(8) {
+        unsafe {
+            let t = _mm256_load_si256(from.add(B/2+i) as *const __m256i);
+            _mm256_store_si256(to.add(i) as *mut __m256i, t);
+            _mm256_store_si256(from.add(B/2+i) as *mut __m256i, infs);
+        }
+    }
+}
+
+#[repr(C, align(64))]
+struct Precalc {
+    mask: [[i32; B]; B]
+}
+
+impl Precalc {
+    const fn new() -> Self {
+        let mut mask = [[0; B]; B];
+
+        let mut i = 0;
+
+        while (i < B) {
+            let mut j = i;
+            while (j < B-1) {
+                mask[i][j] = -1;
+                j += 1;
+            }
+            i += 1;
+        }
+
+        Self {
+            mask
+        }
+    }
+}
+
+const P: Precalc = Precalc::new();
+
+fn insert(node: *mut i32, i: i32, x: i32) {
+    for j in (0..=B-8).rev().step_by(8) {
+        unsafe {
+            let t = _mm256_load_si256(node.add(j) as *const __m256i);
+            let mask = _mm256_load_si256(P.mask[i as usize].as_ptr().add(j) as *const __m256i);      
+            _mm256_maskstore_epi32(node.add(j+1), mask, t);
+        }
+    }
+    unsafe {
+        *node.add(i as usize) = x;
+    }
+}
 
 #[repr(C, align(64))]
 struct BTreeMap {
@@ -68,6 +121,15 @@ impl BTreeMap {
 
         self.tree[k + i]
     }
+
+    // // Node level insert
+    // fn insert(node: *const node, i: i32, x: i32) {
+    //     for j in (0..B-8).rev() {
+    //         let t = unsafe{_mm256_load_si256(&node[j] as *const __m256i)};
+    //         let mask = unsafe {_mm256_load_si256(&P.mask[i][j] as *const __m256i)};
+    //         _mm256_maskstore 
+    //     }
+    // }
 }
 
 #[cfg(test)]
@@ -83,7 +145,7 @@ mod tests {
     fn test_cmp() {
         // Set first 8 elements to 1 to 8 (inclusive)
         let mut array = TestArray([0; 64]);
-        array.0[0..8].copy_from_slice(&(1..9).collect::<Vec<i32>>());
+        array.0[0..8].copy_from_slice(&vec![1,2,3,4,5,6,7,8]);
 
         let mut rng = rand::thread_rng();
 
@@ -133,5 +195,26 @@ mod tests {
                 assert_eq!((i - 1) as u32, count_lt_i);
             }
         }
+    }
+
+    #[test]
+    fn test_move_latter_half() {
+        // Set first 32 elements to 1 to 32 (inclusive)
+        let mut array = TestArray([0; 64]);
+        array.0[0..32].copy_from_slice(&(1..33).collect::<Vec<i32>>());
+        // Set next 32 elements to i32::MAX
+        array.0[32..64].copy_from_slice(&vec![i32::MAX; 32]);
+
+        let array_ptr = array.0.as_mut_ptr();
+
+        move_latter_half(array_ptr, unsafe{array_ptr.add(B)});
+
+        let mut first_half_correct = (1..17).collect::<Vec<i32>>();
+        first_half_correct.append(&mut vec![i32::MAX; 16]);
+        let mut second_half_correct = (17..33).collect::<Vec<i32>>();
+        second_half_correct.append(&mut vec![i32::MAX; 16]);
+
+        assert_eq!(&first_half_correct, &array.0[0..32]);
+        assert_eq!(&second_half_correct, &array.0[32..64]);
     }
 }
